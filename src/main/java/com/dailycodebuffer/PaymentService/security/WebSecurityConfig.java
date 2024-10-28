7e2faa04552014f924057c2e3857bcb525b4a627
Prasad Dhanikonda <prasadsource@gmail.com>
2024-10-28 21:20:34 +0530

    security code disabled

--- src/main/java/com/dailycodebuffer/PaymentService/security/WebSecurityConfig.java
+++ src/main/java/com/dailycodebuffer/PaymentService/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.dailycodebuffer.PaymentService.security;
+/*package com.dailycodebuffer.PaymentService.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,3 +24,4 @@ public class WebSecurityConfig {
         return http.build();
     }
 }
+*/
